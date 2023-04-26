87dce6d844ccfe929cb343405308efc41688fda8
pablorejo <pablopiorejoiglesias@gmail.com>
2023-04-26 13:26:04 +0200

    Añadiendo los cambios de carlos de pedido servicios

--- angular2/src/app/services/pedidos/pedido.service.ts
+++ angular2/src/app/services/pedidos/pedido.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 //import { PedidoModule } from '../../models/pedido/pedido.module';
+import { CocheModule } from 'src/app/models/coche/coche.module';
+import { pathsToSkip } from 'mongoose';
+import { PedidoModule } from 'src/app/models/pedido/pedido.module';
 
 const axios = require('axios');
 const uri = "http://localhost:3000/pedidos"
@@ -12,28 +15,55 @@ const uri_coches = "http://localhost:3000/coches";
 export class PedidoService {
   //selectedPedido: PedidoModule;
   //pedidos: PedidoModule[] = [];
+  coches: CocheModule[] = [];
 
 
   constructor(private http: HttpClient) { 
     //this.selectedPedido = new PedidoModule();
   } 
 
-  async GetCoches(){
+  async obtenerGetCoches(){
     try{
-      const response = await axios.get(uri_coches);
+      var response = await axios.get(uri_coches);
       return response.data;
     }catch(error){
       console.error(error);
+      return -1;
     }
   }
 
-  async DeleteCoche(id_coche : string){
+  async obtenerDeleteCoche(id_coche : string){
     try{
-      const response = await axios.delete(uri_coches + '/' + id_coche);
+      var response = await axios.delete(uri_coches + `/${id_coche}`);
+      return response;
       console.log(response.data);
     }catch(error){
+      return -1;
       console.error(error);
     }
   }
-}
 
+  async obtenerFindByID(id_coche : string){
+    try{
+      var response = await axios.get(uri_coches + `/${id_coche}`);
+      return response;
+    }catch(error){
+      console.error(error);
+      return -1;
+    }
+  } //FINDBYID CREO QUE ESTÁ MAL (YA ARREGLADO)
+
+  async obtenerPutCoche(coche: CocheModule){
+    try{
+      var response = await axios.put(uri_coches + `/${coche._id}`, coche);
+      return response;
+    }catch(error){
+      console.error(error);
+      return -1;
+    }
+  }
+
+  addPedido(pedido :  PedidoModule){
+    return this.http.post(uri, pedido);
+  }
+}
